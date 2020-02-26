1c9e475551aafda172743463a0e47d5da7d48af3
Nicolas Timm <nntimm1@gmail.com>
2020-02-26 08:57:35 -0600

    built stylesheet, and HTML page. outlined 6 dice and table for score totals. input rules starter rules will refind later. set player variables

---
+++ gamePlay.js
@@ -0,0 +1,13 @@
+"use strict";
+let playerOne = 0;
+let playerTwo = 0;
+let playerThree = 0;
+let playerFour = 0;
+let playerFive = 0;
+let playerSix = 0;
+let playerSeven = 0;
+let playerEight = 0;
+let playerNine = 0;
+let playerTen = 0;
+
+
