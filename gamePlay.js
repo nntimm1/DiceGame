9ee7d3ba3b3a5a33cb1060649091053a6b7bb658
Nicolas Timm <nntimm1@gmail.com>
2020-02-26 10:18:36 -0600

    edited table for correct labels and added dice total function and random gen for all dice styles

--- gamePlay.js
+++ gamePlay.js
@@ -1,13 +1,40 @@
 "use strict";
-let playerOne = 0;
-let playerTwo = 0;
-let playerThree = 0;
-let playerFour = 0;
-let playerFive = 0;
-let playerSix = 0;
-let playerSeven = 0;
-let playerEight = 0;
-let playerNine = 0;
-let playerTen = 0;
+let playerOneScore = 0;
+let playerTwoScore = 0;
+let playerThreeScore = 0;
+let playerFourScore = 0;
+let playerFiveScore = 0;
+let playerSixScore = 0;
+let playerSevenScore = 0;
+let playerEightScore = 0;
+let playerNineScore = 0;
+let playerTenScore = 0;
 
+let playerOneStatus = false;
+let playerTwoStatus = false;
+let playerThreeStatus = false;
+let playerFourStatus = false;
+let playerFiveStatus = false;
+let playerSixStatus = false;
+let playerSevenStatus = false;
+let playerEightStatus = false;
+let playerNineStatus = false;
+let playerTenStatus = false;
 
+let dice4 = Math.min(Math.max(parseInt(number), 1), 4);
+let dice6 = Math.min(Math.max(parseInt(number), 1), 6);
+let dice8 = Math.min(Math.max(parseInt(number), 1), 8);
+let dice10 = Math.min(Math.max(parseInt(number), 1), 10);
+let dice12 = Math.min(Math.max(parseInt(number), 1), 12);
+let dice20 = Math.min(Math.max(parseInt(number), 1), 20);
+
+function rollDice() {
+    
+}
+
+function addTotalDice (dice4,dice6,dice8,dice10,dice12,dice20){
+    let result = dice4 + dice6 + dice8 + dice10 + dice12 + dice20;
+    return result;
+}
+
+addTotalDice ()
